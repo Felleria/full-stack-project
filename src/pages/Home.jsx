042d58b507d13ad4afd48979410962c33483d6e4
Felleria <tellytabbie@gmail.com>
2024-07-13 15:54:23 +0300

    updating files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
-// src/pages/Home.jsx
+
 import React from 'react';
 /*import './styles.css';*/
 
 const Home = () => {
   return (
     <div className="container">
-      <h1>Welcome to the JobDeed!</h1>
+      <h1>Welcome to JobDeed!</h1>
       <p>Find your dream job or post job listings to find the perfect candidate.</p>
     </div>
   );
