3afe5c685de907d3da9a1e22f8b3df70cc7a193d
Felleria <tellytabbie@gmail.com>
2024-07-13 15:48:51 +0300

    updating files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-// src/components/Navbar.jsx
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 /*import './styles.css';*/
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">Job Board</Link>
+      <Link className="navbar-brand" to="/">JobDeed</Link>
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
